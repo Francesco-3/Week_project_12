8009072f9247f56d3a4ae0c7c00e3b5fb9d7c866
Francesco Dicecca <147667176+Francesco-3@users.noreply.github.com>
2025-09-26 11:14:29 +0200

    up

---
+++ src/main/java/francescodicecca/Application.java
@@ -0,0 +1,15 @@
+package francescodicecca;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("book_catalog");
+
+    public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+
+        System.out.println("Hello World!");
+    }
+}
